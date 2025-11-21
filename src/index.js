8fccb5567d70ea6eeca673869060cbaeb5f706b2
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-21 23:15:26 +0530

    change code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import History from './components/History/History'
+import Home from './components/Home/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />
+      },
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
